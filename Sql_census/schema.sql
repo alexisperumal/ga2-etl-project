b4ef6feae0372aea3df1102cadc69920acc14f89
Grant Thompson <grant.f.thompson@gmail.com>
2020-02-22 13:22:52 -0800

    schema

--- Sql_census/schema.sql
+++ Sql_census/schema.sql
@@ -1,31 +1,30 @@
-
-CREATE TABLE "Airbnb" (
-    "id" int   NOT NULL,
-    "name" VARCHAR(50)   NOT NULL,
-    "host_id" int   NOT NULL,
-    "host_name" VARCHAR(50)   NOT NULL,
-    "neighbourhood_group" VARCHAR(50)   NOT NULL,
-    "neighbourhood" VARCHAR(50)   NOT NULL,
-    "latitude" float   NOT NULL,
-    "longitude" float   NOT NULL,
-    "room_type" VARCHAR(50)   NOT NULL,
-    "price" int   NOT NULL,
-    "CensusTract" int   NOT NULL
+CREATE TABLE Airbnb (
+  id int  NOT NULL,
+  name VARCHAR(50)  NOT NULL,
+  host_id int  NOT NULL,
+  host_name VARCHAR(50)  NOT NULL,
+  neighbourhood_group VARCHAR(50)  NOT NULL,
+  neighbourhood VARCHAR(50)  NOT NULL,
+  latitude float  NOT NULL,
+  longitude float  NOT NULL,
+  room_type VARCHAR(50)  NOT NULL,
+  price int  NOT NULL,
+  censustract int  NOT NULL
 );
 
-CREATE TABLE "Census_Data" (
-    "CensusTract" int   NOT NULL,
-    "County" VARCHAR(50)   NOT NULL,
-    "Borough" VARCHAR(50)   NOT NULL,
-    "TotalPop" int   NOT NULL,
-    "Hispanic" float   NOT NULL,
-    "White" float   NOT NULL,
-    "Black" float   NOT NULL,
-    "Native" float   NOT NULL,
-    "Asian" float   NOT NULL,
-    "Income" float   NOT NULL,
-    "IncomePerCap" float   NOT NULL,
-    "Poverty" float   NOT NULL,
-    "Employed" int   NOT NULL,
-    "Unemployment" float   NOT NULL
+CREATE TABLE Census_Data (
+   CensusTract BigInt  NOT NULL,
+   County VARCHAR(50) ,
+   Borough VARCHAR(50) ,
+   TotalPop Int ,
+   Hispanic float ,
+   White float,
+   Black float,
+   Native float,
+   Asian float ,
+   Income float,
+   IncomePerCap float,
+   Poverty float ,
+   Employed int,
+   Unemployment float
 );
