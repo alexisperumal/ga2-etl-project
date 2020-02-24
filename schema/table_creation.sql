fd879e60c657556cbae159392dc3276fd0a6cf11
Arundhati Chakraborty <arun@Arundhatis-MacBook-Pro.local>
2020-02-24 14:04:42 -0800

    update the airbnb with censustrack and schema

--- schema/table_creation.sql
+++ schema/table_creation.sql
@@ -1,16 +1,17 @@
-CREATE TABLE Airbnb (
-  id int  NOT NULL,
-  name VARCHAR(50)  NOT NULL,
-  host_id int  NOT NULL,
-  host_name VARCHAR(50)  NOT NULL,
-  neighbourhood_group VARCHAR(50)  NOT NULL,
-  neighbourhood VARCHAR(50)  NOT NULL,
-  latitude float  NOT NULL,
-  longitude float  NOT NULL,
-  room_type VARCHAR(50)  NOT NULL,
-  price int  NOT NULL,
-  CensusTract int  NOT NULL
+CREATE TABLE  airbnb_data (
+  id  int   NOT NULL,
+  name TEXT,
+  host_id  int   NOT NULL,
+  host_name TEXT,
+  neighbourhood_group TEXT,
+  neighbourhood TEXT,
+  latitude float,
+  longitude float,
+  room_type  TEXT,
+  price  int   NOT NULL,
+  censustract  BigInt  NOT NULL
 );
+
 
 CREATE TABLE Census_Data (
    CensusTract BigInt  NOT NULL,
